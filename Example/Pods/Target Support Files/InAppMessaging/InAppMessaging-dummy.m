7d745479cc692d7a8de04e493f191ebe6f744927
Daniel Tam <ts-daniel.a.tam@rakuten.com>
2018-04-11 14:08:47 -0700

    Added new files

---
+++ Example/Pods/Target Support Files/InAppMessaging/InAppMessaging-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_InAppMessaging : NSObject
+@end
+@implementation PodsDummy_InAppMessaging
+@end
---
+++ Example/Tests/Tests.swift
@@ -0,0 +1,104 @@
+/**
+ *  Test class for ConfigurationClient.swift
+ */
+
+import XCTest
+@testable import InAppMessaging
+
+class ConfigurationClientTests: XCTestCase {
+    
+    let stubDataForRetrieveFromMainBundle: [String: Any?] = [
+        "RakutenInsightsConfigURL": "Catfish with Fashion",
+        "ReturnNil": nil
+    ]
+    
+    let stubDataForCallServer = [
+        [
+            "data": [
+                "enabled": true
+            ]
+        ],
+        [
+            "data": [
+                "enabled": false
+            ]
+        ]
+    ]
+    
+    /**
+     * Mock class of CommonUtility. Purpose is to stub the method
+     * retrieveFromMainBundle() to return predefined values in order to test
+     * create behavior of other classes that utilizes CommonUtlity.
+     */
+    private class MockCommonUtility: CommonUtility {
+        
+        let strToRetrieve: String
+        let stubRetrieveFromMainBundle: [String: Any?]
+        let stubCallServer: [String: Any]?
+        
+        init(strToRetrieve: String, stubRetrieveFromMainBundle: [String: Any?], stubCallServer: [String: Any]?) {
+            self.strToRetrieve = strToRetrieve
+            self.stubRetrieveFromMainBundle = stubRetrieveFromMainBundle
+            self.stubCallServer = stubCallServer
+        }
+        
+        override func retrieveFromMainBundle(forKey: String) -> Any? {
+            let dict = self.stubRetrieveFromMainBundle as? NSDictionary
+            return dict?[strToRetrieve] as? String
+        }
+        
+        override func callServer(withUrl: String, withHTTPMethod: String) -> [String: Any]? {
+            return stubCallServer
+        }
+    }
+    
+    /**
+     * Tests for the correct behavior of ConfigurationClient's checkConfigurationServer().
+     * Based on two variables return from retrieveFromMainBundle() and callServer().
+     * SDK should be true if and only if both variables returns something valid.
+     */
+    func testCheckConfigurationServer1() {
+        // True and true case
+        let configurationClient = ConfigurationClient(commonUtility:
+            MockCommonUtility(
+                strToRetrieve: "RakutenInsightsConfigURL",
+                stubRetrieveFromMainBundle: stubDataForRetrieveFromMainBundle,
+                stubCallServer: stubDataForCallServer[0]))
+        
+        XCTAssertTrue(configurationClient.checkConfigurationServer())
+    }
+    
+    func testCheckConfigurationServer2() {
+        // True and false case
+        let configurationClient = ConfigurationClient(commonUtility:
+            MockCommonUtility(
+                strToRetrieve: "RakutenInsightsConfigURL",
+                stubRetrieveFromMainBundle: stubDataForRetrieveFromMainBundle,
+                stubCallServer: stubDataForCallServer[1]))
+        
+        XCTAssertFalse(configurationClient.checkConfigurationServer())
+    }
+    
+    func testCheckConfigurationServer3() {
+        // False and true case
+        let configurationClient = ConfigurationClient(commonUtility:
+            MockCommonUtility(
+                strToRetrieve: "ReturnNil",
+                stubRetrieveFromMainBundle: stubDataForRetrieveFromMainBundle,
+                stubCallServer: stubDataForCallServer[0]))
+        
+        XCTAssertFalse(configurationClient.checkConfigurationServer())
+    }
+    
+    func testCheckConfigurationServer4() {
+        // False and false case
+        let configurationClient = ConfigurationClient(commonUtility:
+            MockCommonUtility(
+                strToRetrieve: "ReturnNil",
+                stubRetrieveFromMainBundle: stubDataForRetrieveFromMainBundle,
+                stubCallServer: stubDataForCallServer[1]))
+        
+        XCTAssertFalse(configurationClient.checkConfigurationServer())
+    }
+}
+
