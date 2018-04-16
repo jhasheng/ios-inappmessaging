196c42d37380219afb4e663f31149fa8c20b9935
Daniel Tam <ts-daniel.a.tam@rakuten.com>
2018-04-16 16:39:14 -0700

    Added Swinject

--- InAppMessaging/Classes/InAppMessaging.swift
+++ InAppMessaging/Classes/InAppMessaging.swift
@@ -2,11 +2,16 @@
  * Class that contains the public methods for host application to call.
  */
 
+import Swinject
+
 /**
  * Function to called by host application to initialize Rakuten InAppMessaging SDK.
  */
 public func configure() {
     let configurationClient = ConfigurationClient(commonUtility: CommonUtility())
+    let container = Container()
+    container.register(name:ConfigurationClient.self, factory: { _ in CommonUtility()})
+    container.register
     
     // (TODO: Daniel Tam) remove if statement later.
     if configurationClient.checkConfigurationServer() {
