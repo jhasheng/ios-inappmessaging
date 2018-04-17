a0c64f5ab8919b66455b19ebdb882bd9806d1db7
Daniel Tam <ts-daniel.a.tam@rakuten.com>
2018-04-17 14:32:17 -0700

    Refactored with Swinject

--- InAppMessaging/Classes/InAppMessaging.swift
+++ InAppMessaging/Classes/InAppMessaging.swift
@@ -4,45 +4,21 @@
 
 import Swinject
 
-class Animal { }
-class Cat: Animal { }
-
-class PetOwner {
-    let pet: Animal
-    init(pet: Animal) {
-        self.pet = pet
-    }
-}
-
-
 public class InAppMessaging {
     
     /**
      * Function to called by host application to configure Rakuten InAppMessaging SDK.
      */
     public class func configure() {
-        let configurationClient = ConfigurationClient(commonUtility: CommonUtility())
-        let container = Container()
         
-        container.register(ConfigurationClient.self) { (resolver) in
+        // Container of ConfigurationClient with added dependency.
+        let configurationClientContainer = Container()
+        configurationClientContainer.register(ConfigurationClient.self) { _ in
             ConfigurationClient(commonUtility: CommonUtility())
         }
         
-        let dis = container.resolve(ConfigurationClient.self)!
-        
-        print(dis.checkConfigurationServer())
-        
-//        container.register(Animal.self) { _ in
-//            Cat()
-//            }.inObjectScope(.container)
-//
-        
-//        // Anywhere later
-//        let anya = container.resolve(PetOwner.self)!
-//        let yoichi = container.resolve(PetOwner.self)!
-//
-//        print(anya !== yoichi)
-//        print(anya.pet === yoichi.pet)
+        // Resolve container into a ConfigurationClient instance.
+        let configurationClient = configurationClientContainer.resolve(ConfigurationClient.self)!
         
         // (TODO: Daniel Tam) remove if statement later.
         if configurationClient.checkConfigurationServer() {
@@ -50,21 +26,6 @@ public class InAppMessaging {
         } else {
             print("Disable SDK.")
         }
-        //semaphore.signal()
-        
     }
-    
-//    @objc class func initializeSdk(semaphore: DispatchSemaphore) {
-//        let configurationClient = ConfigurationClient(commonUtility: CommonUtility())
-//        print(Thread.current)
-//
-//        // (TODO: Daniel Tam) remove if statement later.
-//        if configurationClient.checkConfigurationServer() {
-//            print("Enable SDK.")
-//        } else {
-//            print("Disable SDK.")
-//        }
-//        //semaphore.signal()
-//    }
 }
 
