cf054e96e4866e4d4bf3fca98e9e4c9469ee73c9
Daniel Tam <ts-daniel.a.tam@rakuten.com>
2018-05-04 11:34:34 -0700

    Addressed comments

--- InAppMessaging/Classes/MessageMixerClient.swift
+++ InAppMessaging/Classes/MessageMixerClient.swift
@@ -10,36 +10,39 @@ class MessageMixerClient {
 
     init(secondsBetweenInterval: Double) {
         self.secondsBetweenInterval = secondsBetweenInterval
-        self.setUpTimer()
+        self.startTimer()
     }
     
     /**
      * Function to retrieve Mixer Server URL then pings the server continously
-     * based on the variable secondsBetweenInterval using Timer API.
+     * based on the variable secondsBetweenInterval using DispatchSourceTimer API.
      */
-    fileprivate func setUpTimer() {
-        guard let mixerServerUrl = commonUtility.retrieveFromMainBundle(forKey: "InAppMessagingMixerServerURL") as? String else {
-            print("Error retrieving InAppMessaging Mixer Server URL")
-            return
+    fileprivate func startTimer() {
+        if self.secondsBetweenInterval > 0 {
+            let queue = DispatchQueue(label: "InAppMessagingQueue", qos: .background, attributes: .concurrent)
+            timer = DispatchSource.makeTimerSource(queue: queue)
+            timer!.schedule(deadline: .now(), repeating: self.secondsBetweenInterval)
+            timer!.setEventHandler {
+                self.pingMixerServer()
+            }
+            timer!.resume()
         }
-        
-        let queue = DispatchQueue(label: "InAppMessagingQueue", qos: .background, attributes: .concurrent)
-        timer = DispatchSource.makeTimerSource(queue: queue)
-        timer!.schedule(deadline: .now(), repeating: self.secondsBetweenInterval)
-        timer!.setEventHandler {
-            self.pingMixerServer(mixerServerUrl)
-        }
-        timer!.resume()
     }
     
     /**
      * The function called by the DispatchSourceTimer created in scheduledTimer().
      * This function handles the HTTP request and parsing the response body.
-     * @param { messageMixerUrl: String } URL of InAppMessaging Mixer server.
      * (TODO: Daniel Tam) Parse for Message Mixer endpoint from Config response.
      */
-    fileprivate func pingMixerServer(_ messageMixerUrl: String) {
-        let response = commonUtility.callServer(withUrl: messageMixerUrl, withHTTPMethod: "POST")
+    fileprivate func pingMixerServer() {
+        guard let mixerServerUrl = commonUtility.retrieveFromMainBundle(forKey: "InAppMessagingMixerServerURL") as? String else {
+            #if DEBUG
+                print("Error retrieving InAppMessaging Mixer Server URL")
+            #endif
+            return
+        }
+        
+        let response = commonUtility.callServer(withUrl: mixerServerUrl, withHTTPMethod: "POST")
 
         //(TODO: Daniel Tam) Handle response of message mixer when scope is clearer.
         print(response)
