2a754f6c019be5cf3f402a790098566db09b7b9d
Daniel Tam <ts-daniel.a.tam@rakuten.com>
2018-08-01 14:05:51 -0700

    Moved comments to protocol block

--- InAppMessaging/Classes/Protocols/PlistManipulable.swift
+++ InAppMessaging/Classes/Protocols/PlistManipulable.swift
@@ -4,8 +4,22 @@
 protocol PlistManipulable {
     static var plistURL: URL { get }
     
+    /**
+     * Save any object into the plist file located in plistURL directory.
+     * @param { plist: Any } object to save.
+     */
     static func savePropertyList(_ plist: Any) throws
+    
+    /**
+     * Loads the plist file located in the plistURL directory.
+     * @returns { [String: [Any]] } dictionary with the plist file's content.
+     * @throws error when plist file cannot be found.
+     */
     static func loadPropertyList() throws -> [String: [Any]]
+    
+    /**
+     * Deletes the InAppMessaging's timestamp plist file.
+     */
     static func deletePropertyList() throws
 }
 
@@ -13,20 +27,12 @@ protocol PlistManipulable {
  * Default implementation of PlistManipulable.
  */
 extension PlistManipulable {
-    /**
-     * Save any object into the plist file located in plistURL directory.
-     * @param { plist: Any } object to save.
-     */
+    
     static func savePropertyList(_ plist: Any) throws {
         let plistData = try PropertyListSerialization.data(fromPropertyList: plist, format: .xml, options: 0)
         try plistData.write(to: plistURL)
     }
     
-    /**
-     * Loads the plist file located in the plistURL directory.
-     * @returns { [String: [Any]] } dictionary with the plist file's content.
-     * @throws error when plist file cannot be found.
-     */
     static func loadPropertyList() throws -> [String: [Any]] {
         let data = try Data(contentsOf: plistURL)
         guard let plist = try PropertyListSerialization.propertyList(from: data, format: nil) as? [String: [Any]] else {
@@ -36,9 +42,6 @@ extension PlistManipulable {
         return plist
     }
     
-    /**
-     * Deletes the InAppMessaging's timestamp plist file.
-     */
     static func deletePropertyList() throws {
         try FileManager.default.removeItem(at: plistURL)
     }
