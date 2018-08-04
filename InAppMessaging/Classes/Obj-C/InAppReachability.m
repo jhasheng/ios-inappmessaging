842587e2f9d52f84cdec9fdaf1902f9e99683657
Daniel Tam <ts-daniel.a.tam@rakuten.com>
2018-08-03 17:18:54 -0700

    Added objc folder

---
+++ InAppMessaging/Classes/Obj-C/InAppReachability.m
@@ -0,0 +1,31 @@
+#import <Foundation/Foundation.h>
+#import <InAppMessaging/InAppMessaging-Swift.h>
+#import <Reachability/Reachability.h>
+
+#import "InAppReachability.h"
+
+// Initialize instance for Reachability API.
+Reachability* reach;
+
+@implementation InAppReachability: NSObject
+
++ (void)load {
+    // Listener for network changes.
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(reachabilityChanged)
+                                                 name:kReachabilityChangedNotification object:nil];
+    
+    reach = [Reachability reachabilityForInternetConnection];
+    [reach startNotifier];
+}
+
++ (void)reachabilityChanged {
+    NetworkStatus remoteHostStatus = [reach currentReachabilityStatus];
+    
+    if (remoteHostStatus != NotReachable)
+    {
+        [InAppMessaging configure];
+    }
+}
+
+@end
