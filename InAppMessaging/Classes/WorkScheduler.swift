2a17ed45ae8a21634af91059b0f9fb9dd6d29c9f
Daniel Tam <ts-daniel.a.tam@rakuten.com>
2018-08-06 15:04:07 -0700

    Added comments

--- InAppMessaging/Classes/WorkScheduler.swift
+++ InAppMessaging/Classes/WorkScheduler.swift
@@ -1,6 +1,14 @@
+/**
+ * Struct to help dispatch function calls at a later time in a concurrent queue.
+ */
 struct WorkScheduler {
     static private let inAppMessagingQueue = DispatchQueue(label: "InAppMessagingQueue", attributes: .concurrent)
 
+    /**
+     * Function to schedule a function to be invoked at a later time.
+     * @param { delay: Int } milliseconds before invoking the function.
+     * @param { closure: Void -> Void } function to be invoked.
+     */
     static func scheduleTask(_ delay: Int, closure: @escaping () -> ()) {
         inAppMessagingQueue.asyncAfter(deadline: .now() + .milliseconds(delay)) {
             closure()
