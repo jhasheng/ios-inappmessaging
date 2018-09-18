dc15714f4d043cad1dbd3e3064aaa2aa527e2d2d
Daniel Tam <ts-daniel.a.tam@rakuten.com>
2018-09-18 15:20:53 -0700

    Cleaned up code

--- InAppMessaging/Classes/Models/Events/AppStartEvent.swift
+++ InAppMessaging/Classes/Models/Events/AppStartEvent.swift
@@ -10,18 +10,6 @@ public class AppStartEvent: Event {
         )
     }
     
-    private enum CodingKeys: String, CodingKey {
-        case eventType
-        case name
-        case timestamp
-        case customAttributes
-    }
-    
-    /**
-     * Override encode() to due to an Codable issue when inheriting.
-     * More information here: https://bugs.swift.org/browse/SR-5125 and
-     * https://stackoverflow.com/questions/44553934/using-decodable-in-swift-4-with-inheritance
-     */
     override public func encode(to encoder: Encoder) throws {
         try super.encode(to: encoder)
     }
