4f78a8ed0f957d62891531689307cdc2972ee257
Daniel Tam <ts-daniel.a.tam@rakuten.com>
2018-10-11 17:06:13 -0700

    Working bridger file to use obj-c classes

---
+++ InAppMessaging/Classes/Obj-C/InAppMessaging-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  InAppMessaging-Bridging-Header.h
+//  Pods
+//
+//  Created by Tam, Daniel a on 10/11/18.
+//
+
+#import "InAppReachability.h"
+
+#ifndef InAppMessaging_Bridging_Header_h
+#define InAppMessaging_Bridging_Header_h
+
+
+#endif /* InAppMessaging_Bridging_Header_h */
