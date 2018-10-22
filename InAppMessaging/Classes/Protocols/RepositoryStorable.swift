99f2d8464c5ab5342491406106610090c8b663df
Daniel Tam <ts-daniel.a.tam@rakuten.com>
2018-10-22 15:20:59 -0700

    Added protocol for storing

---
+++ InAppMessaging/Classes/Protocols/RepositoryStorable.swift
@@ -0,0 +1,10 @@
+protocol RepositoryStorable {
+    // Generic type Item to store elements into a list of objects.
+    associatedtype Item
+    
+    // List of Item objects.
+    var list: [Item] { get set }
+    
+    // Add items into the list.
+    mutating func addItem(item: Item)
+}
