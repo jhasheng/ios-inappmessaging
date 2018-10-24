f54a2f09c22797bb4da67084bba0f72d18e3146a
Daniel Tam <ts-daniel.a.tam@rakuten.com>
2018-10-24 16:54:52 -0700

    Working AND trigger logic

--- InAppMessaging/Classes/CampaignReconciliation.swift
+++ InAppMessaging/Classes/CampaignReconciliation.swift
@@ -21,19 +21,16 @@ struct CampaignReconciliation {
 //        let eventList = EventRepository.list
         
         // Create an unique list of event by their eventType. If it is a 4, it will be by name.
+        let uniqueList = generateUniqueEventList()
         
-        
-        
-
         for campaign in CampaignRepository.list {
-            for trigger in campaign.campaignData.triggers {
-                for event in EventRepository.list {
-                    
-
-                } // Event
-            } // Trigger
-            ReadyCampaignRepository.addCampaign(campaign)
-        } // Campaign
+            if isCampaignReady(campaign, uniqueList) {
+                ReadyCampaignRepository.addCampaign(campaign)
+            }
+        }
+        
+        
+        
         print("TEST: \(ReadyCampaignRepository.list)")
         print("TEST: \(ReadyCampaignRepository.list.count)")
 
@@ -42,21 +39,47 @@ struct CampaignReconciliation {
     /**
      * Helps reconciliation process by creating an list of of unique events to match.
      */
-    private func generateUniqueEventList() {
-        var eventTypes: Set<EventType> = [] // To store event types that aren't custom.
+    private static func generateUniqueEventList() -> (uniqueEventTypes: Set<Int>, uniqueEventNames: Set<String>) {
+        var eventTypes: Set<Int> = [] // To store event types that aren't custom.
         var eventNames: Set<String> = [] // To store custom event type.
         
         for event in EventRepository.list {
             if event.eventType != EventType.custom {
-                eventTypes.insert(event.eventType)
+                eventTypes.insert(event.eventType.rawValue)
             } else {
-                // Grab string name.
+                eventNames.insert(event.eventName)
             }
         }
         
+        return (eventTypes,eventNames)
+ 
+    }
+    
+    /**
+     * Method to check if campaign is ready to be displayed or not.
+     */
+    private static func isCampaignReady(
+        _ campaign: Campaign,
+        _ list: (uniqueEventTypes: Set<Int>, uniqueEventNames: Set<String>)) -> Bool {
+        
+            for trigger in campaign.campaignData.triggers {
+                // If the trigger is not custom, check the uniqueEventTypes list for matches.
+                if trigger.eventType != EventType.custom.rawValue {
+                    if !list.uniqueEventTypes.contains(trigger.eventType) {
+                        return false
+                    }
+                } else {
+                    guard let eventName = trigger.eventName else {
+                        return false
+                    }
+                    
+                    // If the trigger is custom, check the uniqueEventNames list for matching names.
+                    if !list.uniqueEventNames.contains(eventName) {
+                        return false
+                    }
+                }
+            }
+        
+            return true
     }
 }
-
-/**
- * If matched, move to next trigger.
- */
