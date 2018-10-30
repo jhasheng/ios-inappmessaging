dbeb9db3e9c0e903af12f6154886beb6c2325103
Daniel Tam <ts-daniel.a.tam@rakuten.com>
2018-10-30 15:19:15 -0700

    Added impression enum detail

--- InAppMessaging/Classes/Enums/ImpressionEnum.swift
+++ InAppMessaging/Classes/Enums/ImpressionEnum.swift
@@ -1,6 +1,10 @@
 /**
  * Enum of impression events.
  */
-enum Impression {
-    
+enum Impression: Int {
+    case invalid = 0
+    case impression = 1
+    case exitButton = 2
+    case actionOneButton = 3
+    case actionTwoButton = 4
 }
