3f5a3b11e0015668f45ae8889e0463c004458153
Daniel Tam <ts-daniel.a.tam@rakuten.com>
2018-11-07 13:52:34 -0800

    Fixed spacing

--- InAppMessaging/Classes/Protocols/TaskSchedulable.swift
+++ InAppMessaging/Classes/Protocols/TaskSchedulable.swift
@@ -3,11 +3,18 @@ protocol TaskSchedulable {
     // Save a reference to the work item in case of cancellation in the future.
     static var workItemReference: DispatchWorkItem? { get set }
     
+    /**
+     * Schedules a task to be ran after a certain delay. Keeps reference of the DispatchWorkItem
+     * so that the work can be cancelled at any time.
+     * @param { elay: Int } delay before running the task.
+     * @param { task: DispatchWorkItem } a block of code to run after the delay.
+     */
     func scheduleWorkItem(_ delay: Int, task: DispatchWorkItem)
-    
-//    func cancelWorkItem()
 }
 
+/**
+ * Default implementation.
+ */
 extension TaskSchedulable {
     func scheduleWorkItem(_ delay: Int, task: DispatchWorkItem) {
 
@@ -17,10 +24,4 @@ extension TaskSchedulable {
             DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(delay), execute: Self.workItemReference!)
         }
     }
-    
-//    func cancelWorkItem() {
-//        if Self.workItemReference != nil {
-//            Self.workItemReference!.cancel()
-//        }
-//    }
 }
--- InAppMessaging/Classes/WorkScheduler.swift
+++ InAppMessaging/Classes/WorkScheduler.swift
@@ -15,5 +15,4 @@ struct WorkScheduler {
             closure()
         }
     }
-    
 }
