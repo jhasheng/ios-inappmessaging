6c0c4566aedcd71386a25927c7e7d8c560abc308
Daniel Tam <ts-daniel.a.tam@rakuten.com>
2018-11-29 14:19:34 -0800

    Added comments

--- InAppMessaging/Classes/Repositories/DisplayedCampaignRepository.swift
+++ InAppMessaging/Classes/Repositories/DisplayedCampaignRepository.swift
@@ -8,11 +8,17 @@ struct DisplayedCampaignRepository {
     // the campaign has been shown.
     static var map: [String: Int] = [:]
     
+    /**
+     * Append a campaign to the mapping.
+     * If the campaign exists, increase counter by 1.
+     * If it does not exist, set counter to 1.
+     * @param { campaign: Campaign } campaign to add in the mapping.
+     */
     static func addCampaign(_ campaign: Campaign) {
         
         let campaignId = campaign.campaignData.campaignId
         
-        // Increment count by 1 if exists, else set campaign count to 1
+        // Increment counter by 1 if exists, else set campaign count to 1
         if map.keys.contains(campaignId) {
             map[campaignId]! += 1
         } else {
@@ -20,6 +26,11 @@ struct DisplayedCampaignRepository {
         }
     }
     
+    /**
+     * Fetches the impression count for a specific campaign
+     * @param { campaign: Campaign } campaign to fetch impression count for.
+     * @returns { Int } number of times the campaign has been shown.
+     */
     static func getDisplayCount(forCampaign campaign: Campaign) -> Int {
         return map[campaign.campaignData.campaignId] ?? 0
     }
