262b0f67df8800eb9108eaec5b912260e48829b1
Daniel Tam <daniel.a.tam@rakuten.com>
2019-01-11 14:02:06 -0800

    Added trigger satisfication check

--- InAppMessaging/Classes/CampaignReconciliation.swift
+++ InAppMessaging/Classes/CampaignReconciliation.swift
@@ -34,6 +34,8 @@ struct CampaignReconciliation {
                 break
             }
             
+            
+            
             // Find out how many times are the triggers satisfied for this campaign.
             // Add to ReadyCampaignRepo.
 //            for _ in getNumberOfTimesToDisplay(campaign) {
@@ -44,7 +46,7 @@ struct CampaignReconciliation {
         }
         
         // Create an unique list of event by their eventType and eventName.
-        let uniqueList = generateUniqueEventList()
+//        let uniqueList = generateUniqueEventList()
         
         // Loop through every campaign in the list and check if it is ready to be displayed.
 //        for campaign in CampaignRepository.list {
@@ -55,28 +57,34 @@ struct CampaignReconciliation {
     }
     
     private static func getNumberOfTimesToDisplay(_ campaign: Campaign) -> Int {
+        var timesToDisplay = 0
+        let maxImpressions = campaign.campaignData.maxImpressions
+        
+        
+        // Max impression - times show already = Remaining times to display
+        //
         
         return 0
     }
     
-    /**
-     * Helps reconciliation process by creating a set of unique eventType and eventName to match.
-     * @returns { (uniqueEventTypes: Set<Int>, uniqueEventNames: Set<String>) } Tuple of unique event types and names set.
-     */
-    private static func generateUniqueEventList() -> (uniqueEventTypes: Set<Int>, uniqueEventNames: Set<String>) {
-        var eventTypes: Set<Int> = [] // To store event types that aren't custom.
-        var eventNames: Set<String> = [] // To store custom event names.
-        
-        for event in EventRepository.list {
-            if event.eventType != EventType.custom {
-                eventTypes.insert(event.eventType.rawValue)
-            } else {
-                eventNames.insert(event.eventName)
-            }
-        }
-        
-        return (eventTypes,eventNames)
-    }
+//    /**
+//     * Helps reconciliation process by creating a set of unique eventType and eventName to match.
+//     * @returns { (uniqueEventTypes: Set<Int>, uniqueEventNames: Set<String>) } Tuple of unique event types and names set.
+//     */
+//    private static func generateUniqueEventList() -> (uniqueEventTypes: Set<Int>, uniqueEventNames: Set<String>) {
+//        var eventTypes: Set<Int> = [] // To store event types that aren't custom.
+//        var eventNames: Set<String> = [] // To store custom event names.
+//
+//        for event in EventRepository.list {
+//            if event.eventType != EventType.custom {
+//                eventTypes.insert(event.eventType.rawValue)
+//            } else {
+//                eventNames.insert(event.eventName)
+//            }
+//        }
+//
+//        return (eventTypes,eventNames)
+//    }
     
     /**
      * Method to check if campaign is ready to be displayed or not.
@@ -88,43 +96,43 @@ struct CampaignReconciliation {
      * @param { (uniqueEventTypes: Set<Int>, uniqueEventNames: Set<String>) } the tuple of unique eventTypes/eventNames.
      * @param { Bool } true if campaign is ready to be displayed, else false.
      */
-    private static func isCampaignReady(
-        _ campaign: Campaign,
-        _ list: (uniqueEventTypes: Set<Int>, uniqueEventNames: Set<String>)) -> Bool {
-        
-            // Secondary check for test campaigns. If theres no triggers, it has to be a test campaign.
-            guard let triggers = campaign.campaignData.triggers else {
-                return true
-            }
-        
-            // If the campaign has reached max impression count within a session, don't show it again.
-            if isMaxImpressionReached(forCampaign: campaign) {
-                return false
-            }
-        
-            // Check for the number of times the triggers are satisfied.
-        
-        
-            for trigger in triggers {
-                // If the trigger is not custom, check the uniqueEventTypes list for matches.
-                if trigger.eventType != EventType.custom.rawValue {
-                    if !list.uniqueEventTypes.contains(trigger.eventType) {
-                        return false
-                    }
-                } else {
-                    guard let eventName = CampaignParser.getCustomEventName(trigger) else {
-                        return false
-                    }
-                    
-                    // If the trigger is custom, check the uniqueEventNames list for matching names.
-                    if !list.uniqueEventNames.contains(eventName) {
-                        return false
-                    }
-                }
-            }
-        
-            return true
-    }
+//    private static func isCampaignReady(
+//        _ campaign: Campaign,
+//        _ list: (uniqueEventTypes: Set<Int>, uniqueEventNames: Set<String>)) -> Bool {
+//
+//            // Secondary check for test campaigns. If theres no triggers, it has to be a test campaign.
+//            guard let triggers = campaign.campaignData.triggers else {
+//                return true
+//            }
+//
+//            // If the campaign has reached max impression count within a session, don't show it again.
+//            if isMaxImpressionReached(forCampaign: campaign) {
+//                return false
+//            }
+//
+//            // Check for the number of times the triggers are satisfied.
+//
+//
+//            for trigger in triggers {
+//                // If the trigger is not custom, check the uniqueEventTypes list for matches.
+//                if trigger.eventType != EventType.custom.rawValue {
+//                    if !list.uniqueEventTypes.contains(trigger.eventType) {
+//                        return false
+//                    }
+//                } else {
+//                    guard let eventName = CampaignParser.getCustomEventName(trigger) else {
+//                        return false
+//                    }
+//
+//                    // If the trigger is custom, check the uniqueEventNames list for matching names.
+//                    if !list.uniqueEventNames.contains(eventName) {
+//                        return false
+//                    }
+//                }
+//            }
+//
+//            return true
+//    }
     
     /**
      * Verifies that the campaign that is being worked on has not reached its max impression count within a session.
@@ -136,28 +144,72 @@ struct CampaignReconciliation {
     }
     
     /**
-     * Create a hashmap of string to list of events. This helps reconciliation process
+     * Create a hashmap of eventType(int) to list of events. This helps reconciliation process
      * by counting up the times an event has been logged by the host app.
      * @param { eventList: [Event] } the list of event to create a mapping of.
-     * @returns { [String: [Event]] } dictionary of string to list of events.
+     * @returns { [Int: [Event]] } dictionary of string to list of events.
      */
-    private static func createEventMap(_ eventList: [Event]) -> [String: [Event]] {
-        var eventMap = [String: [Event]]()
+    private static func createEventMap(_ eventList: [Event]) -> [Int: [Event]] {
+        var eventMap = [Int: [Event]]()
         
         // Loop through the event list to create the mapping.
         for event in eventList {
-            let eventName = event.eventName
+            let eventType = event.eventType.rawValue
 
             // If the eventName already exist in the mapping, append the element.
-            if eventMap.keys.contains(eventName) {
-                eventMap[eventName]?.append(event)
+            if eventMap.keys.contains(eventType) {
+                eventMap[eventType]?.append(event)
             } else {
                 // else, create a new field and list.
-                eventMap[eventName] = [event]
+                eventMap[eventType] = [event]
             }
         }
         
         return eventMap
+    }
+    
+    private static func getNumberOfTimesTriggersAreSatisfied(_ campaign: Campaign) -> Int {
+        
+        // Get the list of triggers off the campaign.
+        guard let campaignTriggers = campaign.campaignData.triggers else {
+            #if DEBUG
+                print("InAppMessaging: campaign has no triggers.")
+            #endif
+            return 0
+        }
+        
+        let localEventMapping = createEventMap(EventRepository.list) // Mapping of local events.
+        var campaignTriggerListMapping = [Int: Int]() // Mapping of the counter for each trigger needed for a campaign.
+        
+        for trigger in campaignTriggers {
+            let eventType = trigger.eventType
+            
+            // If a trigger is already added in the mapping, increment it.
+            if campaignTriggerListMapping.keys.contains(trigger.eventType) {
+                campaignTriggerListMapping[eventType]? += 1
+            } else {
+                // else, add a new entry with the counter as 1.
+                campaignTriggerListMapping[eventType] = 1
+            }
+        }
+        
+        var numberOfTimesSatisfied: Int?
+        
+        // Divide the event mapping with satisfiedTriggersCountMapping to
+        // get the number of times the set of triggers are satisfied.
+        // Find the lowest count satisfied trigger by dividing local event mapping and satisfiedTriggersCountMapping.
+        for (eventType, count) in campaignTriggerListMapping {
+            // If the local event mapping contains an entry of the campaign's trigger.
+            if let eventTypeMappingCount = localEventMapping[eventType]?.count {
+                // Swift, by default, rounds down for Int type when dividing.
+                numberOfTimesSatisfied = eventTypeMappingCount / count
+            } else {
+                // else if theres no entry, then the campaign's trigger has not been fully satisfied.
+                return 0
+            }
+        }
+
+        return numberOfTimesSatisfied ?? 0
     }
     
 }
