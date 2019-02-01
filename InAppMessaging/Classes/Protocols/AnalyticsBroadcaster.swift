cc9cbabddc8fad198157a72643a3a15eb529d1b1
Daniel Tam <daniel.a.tam@rakuten.com>
2019-02-01 10:44:42 -0800

    Added nil check for dataObject

--- InAppMessaging/Classes/Protocols/AnalyticsBroadcaster.swift
+++ InAppMessaging/Classes/Protocols/AnalyticsBroadcaster.swift
@@ -1,12 +1,14 @@
 protocol AnalyticsBroadcaster {
-    func sendEventName(_ name: String, _ dataObject: [String: Any])
+    func sendEventName(_ name: String, _ dataObject: [String: Any]?)
 }
 
 extension AnalyticsBroadcaster {
-    func sendEventName(_ name: String, _ dataObject: [String: Any]) {
+    func sendEventName(_ name: String, _ dataObject: [String: Any]?) {
         var parameters = [String: Any]()
         parameters["eventName"] = name
-        parameters["eventData"] = dataObject
+        if let data = dataObject {
+            parameters["eventData"] = dataObject
+        }
         
         let notificationName = Notification.Name("com.rakuten.esd.sdk.events.custom")
         NotificationCenter.default.post(name: notificationName, object: parameters)
