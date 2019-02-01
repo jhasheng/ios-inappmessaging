7e15d54dfb3d92456fe51f0a995b540fac02784a
Daniel Tam <daniel.a.tam@rakuten.com>
2019-01-31 16:38:35 -0800

    Implemented analytics post notification method

--- InAppMessaging/Classes/Protocols/AnalyticsBroadcaster.swift
+++ InAppMessaging/Classes/Protocols/AnalyticsBroadcaster.swift
@@ -1,3 +1,14 @@
 protocol AnalyticsBroadcaster {
-    func sendEventName(_ name: String, _ topLevelDataObject: [String: Any])
+    func sendEventName(_ name: String, _ dataObject: [String: Any])
+}
+
+extension AnalyticsBroadcaster {
+    func sendEventName(_ name: String, _ dataObject: [String: Any]) {
+        var parameters = [String: Any]()
+        parameters["eventName"] = name
+        parameters["eventData"] = dataObject
+        
+        let notificationName = Notification.Name("com.rakuten.esd.sdk.events.custom")
+        NotificationCenter.default.post(name: notificationName, object: parameters)
+    }
 }
